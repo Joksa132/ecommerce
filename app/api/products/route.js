aea183ed9aaa7089c61edbff7d0bf6122ae2f38d
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-05 16:49:36 +0200

    implement route handler for creating products

---
+++ app/api/products/route.js
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../prisma/prisma'
+
+export async function POST(request) {
+  const data = await request.json()
+
+  const { productName, productDesc, productPrice, productCategory, productImage } = data
+
+  const productExists = await prisma.product.findUnique({
+    where: {
+      title: productName
+    }
+  })
+
+  if (productExists) {
+    return NextResponse.json({ error: 'Same product already exists' });
+  }
+
+  const category = await prisma.category.findUnique({
+    where: {
+      name: productCategory
+    }
+  })
+
+  if (!category) {
+    return NextResponse.json({ error: 'Invalid category' });
+  }
+
+  const product = await prisma.product.create({
+    data: {
+      title: productName,
+      description: productDesc,
+      price: productPrice,
+      //picture: productImage,
+      categories: {
+        connect: {
+          id: category.id
+        }
+      }
+    }
+  })
+
+  console.log("Product created")
+
+  return NextResponse.json({ success: true, product });
+
+}
