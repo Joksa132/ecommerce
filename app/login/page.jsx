e08bbb4d42503e4e33fd4e7929ab8bd5aa869df1
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-06 13:19:30 +0200

    update fetch

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -16,7 +16,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await fetch('http://localhost:3000/api/login', {
+      const res = await fetch('http://localhost:3000/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -17,7 +17,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await fetch('http://localhost:3000/api/register', {
+      const res = await fetch('http://localhost:3000/api/user/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
