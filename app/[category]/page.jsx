10df9fc9b61ff45d0d394810e7af205f299bb7ea
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-07 19:27:50 +0200

    add delete button functionality

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -3,11 +3,14 @@
 import { useContext, useEffect, useState } from "react"
 import styles from './category.module.css'
 import { UserContext } from "../context/userContext"
+import Icon from '@mdi/react';
+import { mdiClose } from '@mdi/js';
 
 export default function CategoryProducts({ params }) {
   const { category } = params
   const [products, setProducts] = useState([])
   const { user } = useContext(UserContext)
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     async function fetchProducts() {
@@ -29,9 +32,36 @@ export default function CategoryProducts({ params }) {
     fetchProducts()
   }, [])
 
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/products/delete/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await res.json()
+      setMessage(data.deletedProduct)
+      setProducts(products.filter(product => product.id !== id))
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <h1 style={{ marginTop: "20px" }}>{category}</h1>
+      {message ?
+        <div className={styles["message-container"]}>
+          <span>{message.title} has been deleted!</span>
+          <Icon
+            path={mdiClose}
+            size={1}
+            style={{ cursor: "pointer" }}
+            onClick={() => setMessage(null)}
+          />
+        </div> : <></>}
       <div className={styles['card-container']}>
         {products.map(product => (
           <div className={styles.card} key={product.id}>
@@ -45,7 +75,7 @@ export default function CategoryProducts({ params }) {
               user.role === "ADMIN" ?
                 <div className={styles["admin-actions"]}>
                   <button>Edit</button>
-                  <button>Delete</button>
+                  <button onClick={() => handleDelete(product.id)}>Delete</button>
                 </div>
                 :
                 <button>Add to cart</button>
