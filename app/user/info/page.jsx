4cd5ba62eb32100a71019c7ba534eec248a21f0c
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-07 20:15:50 +0200

    implement basic edit info page

---
+++ app/user/info/page.jsx
@@ -0,0 +1,16 @@
+import styles from './info.module.css'
+
+export default function EditInfo() {
+  return (
+    <div className={styles.container}>
+      <div className="form-container">
+        <div className="form-container-header">
+          <h2>Edit Info</h2>
+        </div>
+        <div className="form-input-container">
+
+        </div>
+      </div>
+    </div>
+  )
+}
