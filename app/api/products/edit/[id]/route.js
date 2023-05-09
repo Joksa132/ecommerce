9212a8986a1a5871c1f70408c0073a040fe7307f
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-09 21:29:17 +0200

    implement update product route handler

---
+++ app/api/products/edit/[id]/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../../../prisma/prisma'
+
+export async function PUT(request) {
+  const { pathname } = new URL(request.url);
+  const id = pathname.split('/').pop();
+  const data = await request.json()
+  const { productName, productDesc, productPrice, productCategory, productImage } = data
+
+  const category = await prisma.category.findUnique({
+    where: {
+      name: productCategory
+    }
+  })
+
+  const updatedProduct = await prisma.product.update({
+    where: {
+      id: parseInt(id)
+    },
+    data: {
+      title: productName,
+      description: productDesc,
+      price: productPrice,
+      picture: productImage,
+      categories: {
+        connect: {
+          id: category.id
+        }
+      }
+    }
+  })
+
+  return NextResponse.json({ success: true, updatedProduct });
+}
