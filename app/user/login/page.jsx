fcd0a51a794b2fea0783b525fb672426989f73e0
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-13 20:59:08 +0200

    remove console.log

--- app/user/login/page.jsx
+++ app/user/login/page.jsx
@@ -24,8 +24,6 @@ export default function Login() {
       })
       const response = await res.json();
 
-      console.log(data)
-
       if (response.error) {
         setError(response.error);
       } else {
