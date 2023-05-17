4e90de84f10498aff6ea6bc9e18b4e70df7920c4
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-17 19:05:54 +0200

    create prisma seed file

---
+++ prisma/seed.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("@prisma/client");
+
+const categories = [
+  { name: "Laptops" },
+  { name: "Phones" },
+  { name: "Tablets" },
+  { name: "Computers" },
+  { name: "Gaming" },
+];
+
+async function seed() {
+  const prisma = new PrismaClient();
+
+  try {
+    for (const category of categories) {
+      const existingCategory = await prisma.category.findUnique({
+        where: { name: category.name },
+      });
+      if (!existingCategory) {
+        await prisma.category.create({
+          data: category,
+        });
+        console.log(`Category "${category.name}" created.`);
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+seed()
