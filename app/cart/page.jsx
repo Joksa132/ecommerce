3cd8efbedd888b030e818b09ea4d30fb72c7871e
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-18 16:58:29 +0200

    add quantity and total price functionality

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,15 +1,36 @@
 "use client"
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../context/cartContext';
 import Link from 'next/link';
 
 export default function Cart() {
   const { cartProducts, removeFromCart } = useContext(CartContext)
+  const [productQuantities, setProductQuantities] = useState({})
+
+  const handleQuantityChange = (event, productId) => {
+    const newQuantities = { ...productQuantities }
+    newQuantities[productId] = event.target.value
+    setProductQuantities(newQuantities)
+  }
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0
+    for (const product of cartProducts) {
+      const quantity = productQuantities[product.id] || 1
+      const productPrice = product.price * quantity
+      totalPrice += productPrice
+    }
+    return totalPrice;
+  }
 
   return (
     <div className="card-outer-container">
       <h1 style={{ marginTop: "20px" }}>Cart</h1>
+      <div className="total-price">
+        <span>Total Price: {calculateTotalPrice()} RSD</span>
+        <button>Order</button>
+      </div>
       {cartProducts.length ?
         <div className="card-container">
           {cartProducts.map(product => (
@@ -20,6 +41,17 @@ export default function Cart() {
               }
               <p>{product.description}</p>
               <span>{product.price} RSD</span>
+              <div className="product-quantity">
+                <label htmlFor="quantity">Quantity:</label>
+                <input
+                  type="number"
+                  value={productQuantities[product.id] || 1}
+                  onChange={(event) => handleQuantityChange(event, product.id)}
+                  min={1}
+                  id='quantity'
+                  style={{ marginBottom: "10px" }}
+                />
+              </div>
               <div className="card-actions">
                 <button onClick={() => removeFromCart(product.id)}>Remove from Cart</button>
               </div>
