0c149a9cdbed5b8a3064259af986c599f18d43d5
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-20 21:06:08 +0200

    implement creating transaction route handler

---
+++ app/api/transactions/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../../prisma/prisma'
+
+export async function POST(request) {
+  try {
+    const data = await request.json()
+    const { userId, products } = data
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        userId,
+        products: {
+          create: products.map((product) => ({
+            productId: product.id,
+            title: product.title,
+            price: parseFloat(product.price),
+            quantity: parseInt(product.quantity),
+          }))
+        }
+      }
+    })
+
+    console.log("Transaction created")
+
+    return NextResponse.json({ success: true, transaction });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/*
+const transaction = await prisma.transaction.create({
+      data: {
+        userId,
+        products: {
+          create: products.map((product) => ({
+            productId: product.id,
+            title: product.title,
+            price: product.price,
+            quantity: product.quantity,
+          }))
+        }
+      }
+    })
+    */
