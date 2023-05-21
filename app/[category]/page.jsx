c4260d7d688a0a0cef2ef38e0fdc958faca7c4ed
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-21 16:58:53 +0200

    add button to login if not logged in

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -74,7 +74,7 @@ export default function CategoryProducts({ params }) {
             }
             <p>{product.description}</p>
             <span>{product.price} RSD</span>
-            {
+            {user ?
               user.role === "ADMIN" ?
                 <div className="card-actions">
                   <Link href={`/dashboard/edit/${product.id}`}>
@@ -92,7 +92,10 @@ export default function CategoryProducts({ params }) {
                       <button onClick={() => removeFromCart(product.id)}>Remove from Cart</button>
                       : <button onClick={() => addToCart(product)}>Add to Cart</button>
                   }
-                </div>
+                </div> :
+              <Link href={'/user/login'}>
+                <button>Login for Actions</button>
+              </Link>
             }
           </div>
         ))}
