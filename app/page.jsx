6a544694b01e491fc8506ce6db0b3e2397a15fb3
Joksa132 <nikolajoksimovic419@gmail.com>
2023-05-31 19:53:59 +0200

    add random product cards

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,35 @@
 "use client"
 
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from './home.module.css';
 import Link from 'next/link';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { UserContext } from './context/userContext';
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState();
+  const [randomProducts, setRandomProducts] = useState([])
+  const { user } = useContext(UserContext)
+
+  useEffect(() => {
+    async function fetchRandomProducts() {
+      try {
+        const res = await fetch('http://localhost:3000/api/products/random', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const data = await res.json();
+        setRandomProducts(data.randomProducts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchRandomProducts()
+  }, [])
 
   const images = [
     {
@@ -68,6 +90,44 @@ export default function Home() {
           </div>
         ))}
       </Carousel>
+      <h2 style={{ marginTop: "20px" }}>Check some of our products</h2>
+      <div className={styles['card-container']}>
+        {randomProducts && randomProducts.length > 0 ? (
+          randomProducts.map(product => (
+            <div className="card" key={product.id}>
+              <span>{product.title}</span>
+              {product.picture &&
+                <img src={product.picture} alt="Product Picture" />
+              }
+              <p>{product.description}</p>
+              <span>{product.price} RSD</span>
+              {user ?
+                user.role === "ADMIN" ?
+                  <div className="card-actions">
+                    <Link href={`/dashboard/edit/${product.id}`}>
+                      <button>Edit</button>
+                    </Link>
+                    <button onClick={() => handleDelete(product.id)}>Delete</button>
+                  </div>
+                  :
+                  <div className="card-actions">
+                    <Link href={`/${category}/${product.id}`}>
+                      <button>View Details</button>
+                    </Link>
+                    {
+                      cartProducts.some((item) => item.id === product.id) ?
+                        <button onClick={() => removeFromCart(product.id)}>Remove from Cart</button>
+                        : <button onClick={() => addToCart(product)}>Add to Cart</button>
+                    }
+                  </div> :
+                <Link href={'/user/login'}>
+                  <button>Login for Actions</button>
+                </Link>
+              }
+            </div>
+          ))
+        ) : <></>}
+      </div>
     </main>
   )
 }
