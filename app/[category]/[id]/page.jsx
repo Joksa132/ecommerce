825bee7319faf935cbfa3338a1fc601f6ae1fcfa
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-07 12:38:20 +0200

    add additional info for product page

--- app/[category]/[id]/page.jsx
+++ app/[category]/[id]/page.jsx
@@ -37,6 +37,17 @@ export default function ProductDetails({ params }) {
       <div className={styles["product-info"]}>
         <h2 style={{ marginBottom: "30px" }}>{product.title}</h2>
         <p style={{ marginBottom: "30px" }}>Description: {product.description}</p>
+        {product.info &&
+          <div className={styles["product-additional-info"]}>
+            <span>RAM memory: {product.info.ram}GB</span>
+            <span>Storage memory: {product.info.storage}</span>
+            <span>Resolution: {product.info.display}</span>
+            <span>Camera: {product.info.camera}</span>
+            <span>Battery: {product.info.battery}mAh</span>
+            <span>Operating system: {product.info.os}</span>
+            <span>Processor: {product.info.processor}</span>
+          </div>
+        }
         <span>Price: {product.price} RSD</span>
         {
           cartProducts.some((item) => item.id === product.id) ?
