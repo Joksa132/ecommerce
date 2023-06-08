14b7ddc36c158b45b2191aad3a9f47c5571ef294
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-08 18:11:59 +0200

    update edit product api to use additional info

--- app/api/products/edit/[id]/route.js
+++ app/api/products/edit/[id]/route.js
@@ -4,8 +4,40 @@ import prisma from '../../../../../prisma/prisma'
 export async function PUT(request) {
   const { pathname } = new URL(request.url);
   const id = pathname.split('/').pop();
-  const data = await request.json()
-  const { productName, productDesc, productPrice, productCategory, productImage } = data
+
+  const formData = await request.formData();
+  const product = formData.get("product")
+  const parsedProduct = JSON.parse(product)
+
+  const category = await prisma.category.findUnique({
+    where: {
+      name: parsedProduct.category
+    }
+  })
+
+  const updatedProduct = await prisma.product.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      title: parsedProduct.title,
+      description: parsedProduct.description,
+      price: parsedProduct.price,
+      picture: parsedProduct?.image,
+      info: parsedProduct.info,
+      categories: {
+        connect: {
+          id: category.id
+        }
+      }
+    }
+  })
+
+  return NextResponse.json({ success: true, updatedProduct });
+}
+
+
+/*
 
   const category = await prisma.category.findUnique({
     where: {
@@ -31,4 +63,4 @@ export async function PUT(request) {
   })
 
   return NextResponse.json({ success: true, updatedProduct });
-}
+  */
