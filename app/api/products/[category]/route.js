573cb5c50aa40af522f3afed74f7196f80e9017e
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-20 21:38:14 +0200

    implement product filters into the api

--- app/api/products/[category]/route.js
+++ app/api/products/[category]/route.js
@@ -2,6 +2,50 @@ import { NextResponse } from 'next/server';
 import prisma from '../../../../prisma/prisma'
 
 export async function GET(request) {
+  const { pathname, searchParams } = new URL(request.url);
+  const category = pathname.split('/').pop();
+
+  const filters = {
+    ram: searchParams.get('ram'),
+    storage: searchParams.get('storage'),
+    display: searchParams.get('display'),
+    os: searchParams.get('os'),
+    battery: searchParams.get('battery'),
+    camera: searchParams.get('camera'),
+    processor: searchParams.get('processor')
+  }
+
+  const products = await prisma.product.findMany({
+    where: {
+      categories: {
+        some: {
+          name: category
+        }
+      }
+    },
+    include: {
+      categories: true,
+    },
+  })
+
+  const filteredProducts = products.filter((product) => {
+    const info = product.info;
+    for (const key in filters) {
+      if (filters[key] && info[key] !== filters[key]) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  return NextResponse.json({ products: filteredProducts, allProducts: products });
+}
+
+/*
+import { NextResponse } from 'next/server';
+import prisma from '../../../../prisma/prisma'
+
+export async function GET(request) {
   const { pathname } = new URL(request.url);
   const category = pathname.split('/').pop();
 
@@ -20,3 +64,4 @@ export async function GET(request) {
 
   return NextResponse.json({ products })
 }
+*/
