a686eeea04af3ff804e1313ab318ce8e96ea7050
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-20 19:16:05 +0200

    add product filters

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -10,6 +10,15 @@ export default function CategoryProducts({ params }) {
   const { category } = params
   const [products, setProducts] = useState([])
   const [message, setMessage] = useState(null)
+  const [filters, setFilters] = useState({
+    ram: '',
+    storage: '',
+    display: '',
+    camera: '',
+    battery: '',
+    os: '',
+    processor: '',
+  })
 
   useEffect(() => {
     async function fetchProducts() {
@@ -48,27 +57,63 @@ export default function CategoryProducts({ params }) {
     }
   }
 
+  const infoFields = ['processor', 'display', 'ram', 'os', 'battery', 'camera', 'storage',];
+  const availableInfoValues = infoFields.reduce((values, field) => {
+    const uniqueValues = Array.from(new Set(products.map(product => product.info?.[field]))).filter(Boolean);
+    return {
+      ...values,
+      [field]: uniqueValues
+    };
+  }, {});
+
+  const handleFilterChange = (field, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [field]: value,
+    }));
+  };
+
+  console.log(filters)
+
   return (
     <div className="card-outer-container">
       <h1 style={{ marginTop: "20px" }}>{category}</h1>
       <div className="filter-container">
-
+        {infoFields.map(field => (
+          <div key={field} className="filter-group">
+            <span>{field}:</span>
+            {availableInfoValues[field]?.map((value) => (
+              <label key={value}>
+                <input
+                  type="checkbox"
+                  checked={filters[field] === value}
+                  onChange={(e) =>
+                    handleFilterChange(field, e.target.checked ? value : '')
+                  }
+                />
+                {value}
+              </label>
+            ))}
+          </div>
+        ))}
       </div>
-      {message ?
-        <div className={styles["message-container"]}>
-          <span>{message.title} has been deleted!</span>
-          <Icon
-            path={mdiClose}
-            size={1}
-            style={{ cursor: "pointer" }}
-            onClick={() => setMessage(null)}
-          />
-        </div> : <></>}
+      {
+        message ?
+          <div className={styles["message-container"]}>
+            <span>{message.title} has been deleted!</span>
+            <Icon
+              path={mdiClose}
+              size={1}
+              style={{ cursor: "pointer" }}
+              onClick={() => setMessage(null)}
+            />
+          </div> : <></>
+      }
       <div className='card-container'>
         {products.map(product => (
           <ProductCard product={product} key={product.id} isCart={false} handleDelete={handleDelete} />
         ))}
       </div>
-    </div>
+    </div >
   )
 }
