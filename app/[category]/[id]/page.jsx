8ee4dbf08c9d4107e117a2e6d2c4b850e9779392
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-26 19:51:38 +0200

    add loading spinner

--- app/[category]/[id]/page.jsx
+++ app/[category]/[id]/page.jsx
@@ -11,6 +11,7 @@ export default function ProductDetails({ params }) {
   const [product, setProduct] = useState({})
   const { cartProducts, addToCart, removeFromCart } = useContext(CartContext)
   const { user } = useContext(UserContext)
+  const [pageLoading, setPageLoading] = useState(true)
 
   useEffect(() => {
     async function fetchProduct() {
@@ -23,6 +24,7 @@ export default function ProductDetails({ params }) {
         })
         const data = await res.json();
         setProduct(data.product)
+        setPageLoading(false)
       }
       catch (error) {
         console.log(error)
@@ -34,36 +36,41 @@ export default function ProductDetails({ params }) {
 
   return (
     <div className={styles.container}>
-      <div className={styles["product-image"]}>
-        <img src={product.picture} alt={product.title} />
-      </div>
-      <div className={styles["product-info"]}>
-        <h2 style={{ marginBottom: "30px" }}>{product.title}</h2>
-        <p style={{ marginBottom: "30px" }}>Description: {product.description}</p>
-        {product.info &&
-          <div className={styles["product-additional-info"]}>
-            <span>RAM memory: {product.info.ram}GB</span>
-            <span>Storage memory: {product.info.storage}</span>
-            <span>Resolution: {product.info.display}</span>
-            <span>Camera: {product.info.camera}</span>
-            <span>Battery: {product.info.battery}mAh</span>
-            <span>Operating system: {product.info.os}</span>
-            <span>Processor: {product.info.processor}</span>
+      {pageLoading ?
+        <div className="loading-spinner"></div> :
+        <>
+          <div className={styles["product-image"]}>
+            <img src={product.picture} alt={product.title} />
           </div>
-        }
-        <span>Price: {product.price} RSD</span>
-        {
-          user ? <>
+          <div className={styles["product-info"]}>
+            <h2 style={{ marginBottom: "30px" }}>{product.title}</h2>
+            <p style={{ marginBottom: "30px" }}>Description: {product.description}</p>
+            {product.info &&
+              <div className={styles["product-additional-info"]}>
+                <span>RAM memory: {product.info.ram}GB</span>
+                <span>Storage memory: {product.info.storage}</span>
+                <span>Resolution: {product.info.display}</span>
+                <span>Camera: {product.info.camera}</span>
+                <span>Battery: {product.info.battery}mAh</span>
+                <span>Operating system: {product.info.os}</span>
+                <span>Processor: {product.info.processor}</span>
+              </div>
+            }
+            <span>Price: {product.price} RSD</span>
             {
-              cartProducts.some((item) => item.id === product.id) ?
-                <button onClick={() => removeFromCart(product.id)} className={styles["cart-button"]}>Remove from Cart</button>
-                : <button onClick={() => addToCart(product)} className={styles["cart-button"]}>Add to Cart</button>}
-          </>
-            : <Link href={'/user/login'}>
-              <button className={styles["login-button"]}>Login to Order</button>
-            </Link>
-        }
-      </div>
+              user ? <>
+                {
+                  cartProducts.some((item) => item.id === product.id) ?
+                    <button onClick={() => removeFromCart(product.id)} className={styles["cart-button"]}>Remove from Cart</button>
+                    : <button onClick={() => addToCart(product)} className={styles["cart-button"]}>Add to Cart</button>}
+              </>
+                : <Link href={'/user/login'}>
+                  <button className={styles["login-button"]}>Login to Order</button>
+                </Link>
+            }
+          </div>
+        </>
+      }
     </div>
   )
 }
