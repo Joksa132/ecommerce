79b51c27d103fc99453a23bf2ab446c9663e49f4
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-05 23:06:16 +0200

    change font to poppins

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
 import Nav from '../components/Nav'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import { UserProvider } from '../context/userContext'
 import { CartProvider } from '../context/cartContext'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: "400" })
 
 export const metadata = {
   title: 'Ecommerce Website',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <UserProvider>
           <CartProvider>
             <Nav />
