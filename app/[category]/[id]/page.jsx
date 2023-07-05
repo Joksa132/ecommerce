c4a4b6a9d7c92defdd04d40499d6e07f356acfa7
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-05 20:47:38 +0200

    fix and update stuff

--- app/[category]/[id]/page.jsx
+++ app/[category]/[id]/page.jsx
@@ -5,6 +5,8 @@ import styles from './product.module.css'
 import { CartContext } from "@/context/cartContext"
 import { UserContext } from "@/context/userContext"
 import Link from "next/link"
+import Icon from '@mdi/react';
+import { mdiMenuDown } from '@mdi/js';
 
 export default function ProductDetails({ params }) {
   const { id } = params
@@ -12,6 +14,8 @@ export default function ProductDetails({ params }) {
   const { cartProducts, addToCart, removeFromCart } = useContext(CartContext)
   const { user } = useContext(UserContext)
   const [pageLoading, setPageLoading] = useState(true)
+  const [descriptionClicked, setDescriptionClicked] = useState(true)
+  const [specificationsClicked, setSpecificationsClicked] = useState(true)
 
   useEffect(() => {
     async function fetchProduct() {
@@ -39,38 +43,59 @@ export default function ProductDetails({ params }) {
       {pageLoading ?
         <div className="loading-spinner"></div> :
         <>
-          <div className={styles["product-image"]}>
-            <img src={product.picture} alt={product.title} />
-          </div>
-          <div className={styles["product-info"]}>
-            <h2 style={{ marginBottom: "30px" }}>{product.title}</h2>
-            <p style={{ marginBottom: "30px" }}>Description: {product.description}</p>
-            {product.info &&
-              <div className={styles["product-additional-info"]}>
-                <span>RAM memory: {product.info.ram}</span>
-                <span>Storage memory: {product.info.storage}</span>
-                <span>Resolution: {product.info.display}</span>
-                <span>Camera: {product.info.camera}</span>
-                <span>Battery: {product.info.battery}</span>
-                <span>Operating system: {product.info.os}</span>
-                <span>Processor: {product.info.processor}</span>
+          <div className={styles["main-container"]}>
+            <div className={styles["product-image"]}>
+              <img src={product.picture} alt={product.title} />
+            </div>
+            <div className={styles["product-info"]}>
+              <h1>{product.title}</h1>
+              <span className={styles["price-info"]}>{product.price} RSD</span>
+              <div className={styles["info-container"]}>
+                <div className={styles["description-info"]}>
+                  <div className={styles["description-button"]} onClick={() => setDescriptionClicked(!descriptionClicked)}>
+                    <button>Description</button>
+                    <Icon path={mdiMenuDown} size={1} />
+                  </div>
+                  {descriptionClicked &&
+                    <p style={{ marginTop: "10px" }}>{product.description}</p>
+                  }
+                </div>
+                <div className={styles["specifications-info"]}>
+                  <div className={styles["description-button"]} onClick={() => setSpecificationsClicked(!specificationsClicked)}>
+                    <button>Specifications</button>
+                    <Icon path={mdiMenuDown} size={1} />
+                  </div>
+                  {specificationsClicked &&
+                    (
+                      product.info &&
+                      <div className={styles["product-additional-info"]}>
+                        <span>RAM memory: {product.info.ram}</span>
+                        <span>Storage memory: {product.info.storage}</span>
+                        <span>Resolution: {product.info.display}</span>
+                        <span>Camera: {product.info.camera}</span>
+                        <span>Battery: {product.info.battery}</span>
+                        <span>Operating system: {product.info.os}</span>
+                        <span>Processor: {product.info.processor}</span>
+                      </div>
+                    )
+                  }
+                </div>
               </div>
-            }
-            <span>Price: {product.price} RSD</span>
-            {
-              user ? <>
-                {
-                  cartProducts.some((item) => item.id === product.id) ?
-                    <button onClick={() => removeFromCart(product.id)} className={styles["cart-button"]}>Remove from Cart</button>
-                    : <button onClick={() => addToCart(product)} className={styles["cart-button"]}>Add to Cart</button>}
-              </>
-                : <Link href={'/user/login'}>
-                  <button className={styles["login-button"]}>Login to Order</button>
-                </Link>
-            }
+              {
+                user ? <>
+                  {
+                    cartProducts.some((item) => item.id === product.id) ?
+                      <button onClick={() => removeFromCart(product.id)} className={styles["cart-button"]}>Remove from Cart</button>
+                      : <button onClick={() => addToCart(product)} className={styles["cart-button"]}>Add to Cart</button>}
+                </>
+                  : <Link href={'/user/login'}>
+                    <button className={styles["login-button"]}>Login to Order</button>
+                  </Link>
+              }
+            </div>
           </div>
         </>
       }
-    </div>
+    </div >
   )
 }
