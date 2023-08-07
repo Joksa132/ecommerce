60c5d8ff097484bee2a868727fb015695623a40a
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-07 18:17:44 +0200

    add more font weights

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import { Poppins } from 'next/font/google'
 import { UserProvider } from '../context/userContext'
 import { CartProvider } from '../context/cartContext'
 
-const poppins = Poppins({ subsets: ['latin'], weight: "400" })
+const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "600", "700", "800", "900"] })
 
 export const metadata = {
   title: 'Ecommerce Website',
