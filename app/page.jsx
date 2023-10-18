b48d790e23f3fbb64c4846541d32c4c543b64e86
Joksa132 <nikolajoksimovic419@gmail.com>
2023-10-18 15:26:07 +0200

    fix random products

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,23 @@
 "use client"
 
-import { useEffect, useState } from 'react';
-import styles from './home.module.css';
-import ProductCard from '../components/ProductCard';
+import { useEffect, useState } from "react"
+import styles from "./home.module.css"
+import ProductCard from "../components/ProductCard"
 
 export default function Home() {
   const [randomProducts, setRandomProducts] = useState([])
 
   useEffect(() => {
     async function fetchRandomProducts() {
+      const timestamp = Date.now()
       try {
-        const res = await fetch(`/api/products/random?timestamp=${Date.now()}`, {
-          method: 'GET',
+        const res = await fetch(`/api/products/random?timestamp=${timestamp}`, {
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json'
-          }
+            "Content-Type": "application/json",
+          },
         })
-        const data = await res.json();
+        const data = await res.json()
         setRandomProducts(data.randomProducts)
         console.log(data)
       } catch (error) {
@@ -27,19 +28,18 @@ export default function Home() {
     fetchRandomProducts()
   }, [])
 
-  const handleDelete = async (id) => {
+  const handleDelete = async id => {
     try {
       const res = await fetch(`/api/products/delete/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       })
       const data = await res.json()
 
       setRandomProducts(randomProducts.filter(product => product.id !== id))
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error)
     }
   }
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <section className={styles["home-container"]}>
       <div className={styles["image-container"]}>
-        <img className={styles['cover-image']} src='/home-page-cover.jpg' alt='Cover image' />
+        <img className={styles["cover-image"]} src="/home-page-cover.jpg" alt="Cover image" />
         <div className="container">
           <span>Brand new online technology store bringing you only the finest of products</span>
         </div>
@@ -57,9 +57,16 @@ export default function Home() {
         <div className={`${styles["card-container"]}`}>
           {randomProducts && randomProducts.length > 0 ? (
             randomProducts.map(product => (
-              <ProductCard product={product} key={product.id} isCart={false} handleDelete={handleDelete} />
+              <ProductCard
+                product={product}
+                key={product.id}
+                isCart={false}
+                handleDelete={handleDelete}
+              />
             ))
-          ) : <></>}
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </section>
