2e6674cd5cbf871741f17a3df1a9b37d0872a5be
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-05 14:32:36 +0100

    remove delete product

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,29 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import { useContext } from "react";
 import { UserContext } from "../context/userContext";
 import { CartContext } from "../context/cartContext";
-import Icon from '@mdi/react';
-import { mdiPlus, mdiMinus } from '@mdi/js';
+import Icon from "@mdi/react";
+import { mdiPlus, mdiMinus } from "@mdi/js";
 
-export default function ProductCard({ product, handleDelete, handleQuantityChange, productQuantities, isCart }) {
-  const { user } = useContext(UserContext)
-  const { cartProducts, addToCart, removeFromCart } = useContext(CartContext)
+export default function ProductCard({
+  product,
+  handleQuantityChange,
+  productQuantities,
+  isCart,
+}) {
+  const { user } = useContext(UserContext);
+  const { cartProducts, addToCart, removeFromCart } = useContext(CartContext);
 
   return (
     <div className="card">
       <div className="card-top">
-        {product.picture &&
+        {product.picture && (
           <div className="card-img-container">
             <img src={product.picture} alt="Product Picture" />
           </div>
-        }
+        )}
         <div className="card-name">
           <Link href={`/${product.categories[0].name}/${product.id}`}>
             <span className="card-product-title">{product.title}</span>
           </Link>
           <span className="card-product-price">{product.price} RSD</span>
         </div>
-        {isCart ?
+        {isCart ? (
           <div className="product-quantity">
             <label htmlFor="quantity">Quantity:</label>
             <input
@@ -31,48 +38,53 @@ export default function ProductCard({ product, handleDelete, handleQuantityChang
               value={productQuantities[product.id] || 1}
               onChange={(event) => handleQuantityChange(event, product.id)}
               min={1}
-              id='quantity'
+              id="quantity"
               style={{ marginBottom: "10px" }}
             />
-          </div> : <></>
-        }
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
       <div className="card-bottom">
         {user ? (
-          user.role === "ADMIN" ?
+          user.role === "ADMIN" ? (
             <div className="card-admin-actions">
               <Link href={`/dashboard/edit/${product.id}`}>
                 <button>Edit</button>
               </Link>
-              <button onClick={() => handleDelete(product.id)}>Delete</button>
-            </div> :
-            <div className="card-actions">
-              {
-                cartProducts.some((item) => item.id === product.id) ?
-                  <button
-                    className="card-cart-button"
-                    onClick={() => removeFromCart(product.id)}
-                    style={{ cursor: "pointer" }}>
-                    <Icon path={mdiMinus} size={1} />
-                    REMOVE FROM CART
-                  </button>
-                  : <button
-                    className="card-cart-button"
-                    onClick={() => addToCart(product)}
-                    style={{ cursor: "pointer" }}>
-                    <Icon path={mdiPlus} size={1} />
-                    ADD TO CART
-                  </button>
-              }
             </div>
-        ) :
+          ) : (
+            <div className="card-actions">
+              {cartProducts.some((item) => item.id === product.id) ? (
+                <button
+                  className="card-cart-button"
+                  onClick={() => removeFromCart(product.id)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <Icon path={mdiMinus} size={1} />
+                  REMOVE FROM CART
+                </button>
+              ) : (
+                <button
+                  className="card-cart-button"
+                  onClick={() => addToCart(product)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <Icon path={mdiPlus} size={1} />
+                  ADD TO CART
+                </button>
+              )}
+            </div>
+          )
+        ) : (
           <div className="card-actions">
-            <Link href={'/user/login'} className="card-login">
+            <Link href={"/user/login"} className="card-login">
               <button>LOGIN TO ORDER</button>
             </Link>
           </div>
-        }
+        )}
       </div>
-    </div >
-  )
+    </div>
+  );
 }
