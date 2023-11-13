9f0a104704ddaaffcc44200ac767585148957554
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-13 19:21:37 +0100

    convert to server component

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,68 +1,47 @@
-"use client"
+import ProductCard from "../../components/ProductCard";
+import prisma from "@/prisma/prisma";
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import ProductCard from '../../components/ProductCard';
+async function getSearchedProduct(query) {
+  const products = await prisma.product.findMany({
+    where: {
+      title: {
+        contains: query,
+        mode: "insensitive",
+      },
+    },
+    include: {
+      categories: true,
+    },
+  });
 
-export default function SearchResults() {
-  const searchParams = useSearchParams();
-  const productQuery = searchParams.get('product');
-  const [products, setProducts] = useState([])
+  return products;
+}
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch(`/api/products/get/all/${productQuery}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        });
-        const data = await res.json();
-
-        if (data.success) {
-          setProducts(data.products);
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    fetchProducts()
-  }, [productQuery])
-
-  const handleDelete = async (id) => {
-    try {
-      const res = await fetch(`/api/products/delete/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const data = await res.json()
-
-      setProducts(products.filter(product => product.id !== id))
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
+export default async function SearchResults({ searchParams }) {
+  let products = await getSearchedProduct(searchParams.product);
 
   return (
-    <div className='card-outer-container container'>
-      <div style={{ display: "flex", alignContent: "center", marginTop: "30px" }}>
-        <h2>Search results for <span style={{ fontStyle: "italic" }}>{productQuery}</span></h2>
+    <div className="card-outer-container container">
+      <div
+        style={{ display: "flex", alignContent: "center", marginTop: "30px" }}
+      >
+        <h2>
+          Search results for{" "}
+          <span style={{ fontStyle: "italic" }}>{searchParams.product}</span>
+        </h2>
       </div>
-      {products.length ?
-        <div className='card-container'>
-          {products.map(product => (
-            < ProductCard product={product} key={product.id} isCart={false} handleDelete={handleDelete} />
+      {products.length ? (
+        <div className="card-container">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} isCart={false} />
           ))}
-        </div> :
-        <div style={{ fontSize: "2rem", marginTop: "30px" }}>
-          No products matching <span style={{ fontWeight: "bold" }}>{productQuery}</span>
         </div>
-      }
+      ) : (
+        <div style={{ fontSize: "2rem", marginTop: "30px" }}>
+          No products matching{" "}
+          <span style={{ fontWeight: "bold" }}>{searchParams.product}</span>
+        </div>
+      )}
     </div>
-  )
+  );
 }
