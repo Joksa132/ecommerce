dd03ceb65a6007fbcd29e7d47b770d6f588e3fc3
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-15 16:38:55 +0100

    convert category page to server component

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -1,49 +1,57 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import CategoryPage from "@/components/CategoryPage";
 import styles from "./category.module.css";
-import Icon from "@mdi/react";
-import { mdiClose, mdiFilterVariant } from "@mdi/js";
-import ProductCard from "../../components/ProductCard";
-import FiltersSidebar from "@/components/FiltersSidebar";
+import prisma from "@/prisma/prisma";
 
-export default function CategoryProducts({ params }) {
-  const { category } = params;
-  const [products, setProducts] = useState([]);
-  const [allProducts, setAllProducts] = useState([]);
-  const [message, setMessage] = useState(null);
-  const [filters, setFilters] = useState({});
-  const [pageLoading, setPageLoading] = useState(true);
-  const [isFiltersClicked, setIsFiltersClicked] = useState(false);
+async function getAllProducts(category) {
+  const products = await prisma.product.findMany({
+    where: {
+      categories: {
+        some: {
+          name: category,
+        },
+      },
+    },
+    include: {
+      categories: true,
+    },
+  });
 
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const queryString = new URLSearchParams(filters).toString();
+  return products;
+}
 
-        const res = await fetch(`/api/products/${category}?${queryString}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+async function getFilteredProducts(allProducts, searchParams) {
+  const filters = {
+    ram: searchParams?.ram,
+    storage: searchParams?.storage,
+    display: searchParams?.display,
+    os: searchParams?.os,
+    battery: searchParams?.battery,
+    camera: searchParams?.camera,
+    processor: searchParams?.processor,
+  };
 
-        const data = await res.json();
-        setProducts(data.products);
-        setAllProducts(data.allProducts);
-        setPageLoading(false);
-      } catch (error) {
-        console.log(error);
+  const filteredProducts = allProducts.filter((product) => {
+    const info = product.info;
+    for (const key in filters) {
+      if (filters[key] && info[key] !== filters[key]) {
+        return false;
       }
     }
+    return true;
+  });
 
-    fetchProducts();
-  }, [filters]);
+  return filteredProducts;
+}
+
+export default async function CategoryProducts({ params, searchParams }) {
+  const { category } = params;
+  const allProducts = await getAllProducts(category);
+  const products = await getFilteredProducts(allProducts, searchParams);
 
   const infoFields = ["ram", "storage", "display", "os", "battery"];
   const availableInfoValues = infoFields.reduce((values, field) => {
     const uniqueValues = Array.from(
-      new Set(allProducts.map(product => product.info?.[field]))
+      new Set(allProducts.map((product) => product.info?.[field]))
     ).filter(Boolean);
     const sortedValues = uniqueValues.sort((a, b) =>
       a.localeCompare(b, undefined, { numeric: true })
@@ -54,70 +62,15 @@ export default function CategoryProducts({ params }) {
     };
   }, {});
 
-  const handleFilterChange = (field, value) => {
-    setFilters(prevFilters => {
-      const updatedFilters = { ...prevFilters };
-
-      if (value) {
-        updatedFilters[field] = value;
-      } else {
-        delete updatedFilters[field];
-      }
-
-      return updatedFilters;
-    });
-  };
-
   return (
     <div className="product-page-container">
-      {pageLoading ? (
-        <div className="loading-spinner"></div>
-      ) : (
-        <div className="card-outer-container container">
-          <div className="product-page-header">
-            <h1>{category}</h1>
-            <span onClick={() => setIsFiltersClicked(true)}>
-              <Icon path={mdiFilterVariant} size={1} />
-              Filter
-            </span>
-          </div>
-          {message ? (
-            <div className={styles["message-container"]}>
-              <span>{message.title} has been deleted!</span>
-              <Icon
-                path={mdiClose}
-                size={1}
-                style={{ cursor: "pointer" }}
-                onClick={() => setMessage(null)}
-              />
-            </div>
-          ) : (
-            <></>
-          )}
-          <div className="card-container">
-            {products.map(product => (
-              <ProductCard product={product} key={product.id} isCart={false} />
-            ))}
-          </div>
-        </div>
-      )}
-      {isFiltersClicked ? (
-        <>
-          <div
-            className="overlay"
-            onClick={() => setIsFiltersClicked(false)}
-          ></div>
-          <FiltersSidebar
-            infoFields={infoFields}
-            availableInfoValues={availableInfoValues}
-            filters={filters}
-            handleFilterChange={handleFilterChange}
-            setIsFiltersClicked={setIsFiltersClicked}
-          />
-        </>
-      ) : (
-        <></>
-      )}
+      <CategoryPage
+        styles={styles}
+        infoFields={infoFields}
+        availableInfoValues={availableInfoValues}
+        category={category}
+        products={products}
+      />
     </div>
   );
 }
